541db240ac3df306f912835631a86311e1bb952b
Nikola Irinchev <irinchev@me.com>
2015-12-04 13:10:05 +0200

    Add names of class

---
+++ BfuDay2/Controllers/NamesController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+
+namespace BfuDay2.Controllers
+{
+    [RoutePrefix("names")]
+    public class NamesController : ApiController
+    {
+        [Route("")]
+        [HttpGet]
+        public IHttpActionResult GetNames()
+        {
+            return this.Ok(new[]
+            {
+                "Niki"
+            });
+        }
+    }
+}
